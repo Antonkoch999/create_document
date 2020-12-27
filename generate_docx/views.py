dabcc4c5e491a5f783f91ac03a7f165886b59757
Anton <antonkoch999@gmail.com>
2020-12-27 16:24:57 +0300

    fix

--- generate_docx/views.py
+++ generate_docx/views.py
@@ -196,6 +196,7 @@ def screenshot(request):
 
     chrome_options = Options()
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
     chrome_options.binary_location = CHROME_PATH
     if request.method == "POST":
